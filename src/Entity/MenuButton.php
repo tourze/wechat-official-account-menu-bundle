48b5cf1247c5538673e7d234768e702c16bf459f
github-ci <you@example.com>
2025-04-28 09:47:07 +0000

    fix: 修复字段错误

--- src/Entity/MenuButton.php
+++ src/Entity/MenuButton.php
@@ -50,7 +50,7 @@ class MenuButton implements \Stringable, Itemable
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[FormField(span: 12)]
     #[ListColumn]
